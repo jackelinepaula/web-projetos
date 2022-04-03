b482839488151935cc1983524dc92bb3253e52e5
Jackeline Paula <kpopcherry123@gmail.com>
2022-04-03 17:30:15 -0300

    e-commerce shoes

--- Rocketshoes/js/script.js
+++ Rocketshoes/js/script.js
@@ -1,43 +1,8 @@
-function ThumbnailPlugin(main) {
-    return (slider) => {
-        function removeActive() {
-            slider.slides.forEach((slide) => {
-                slide.classList.remove("active")
-            })
-        }
-        function addActive(idx) {
-            slider.slides[idx].classList.add("active")
-        }
+let btnMobi = document.querySelector('.menu-hamb')
+let itens = document.querySelector('#itens-responsivo')
 
-        function addClickEvents() {
-            slider.slides.forEach((slide, idx) => {
-                slide.addEventListener("click", () => {
-                    main.moveToIdx(idx)
-                })
-            })
-        }
+btnMobi.addEventListener('click', function(){
+    itens.classList.toggle('active')
+})
 
-        slider.on("created", () => {
-            addActive(slider.track.details.rel)
-            addClickEvents()
-            main.on("animationStarted", (main) => {
-                removeActive()
-                const next = main.animator.targetIdx || 0
-                addActive(main.track.absToRel(next))
-                slider.moveToIdx(next)
-            })
-        })
-    }
-}
-var slider = new KeenSlider("#my-keen-slider")
-var thumbnails = new KeenSlider(
-    "#thumbnails",
-    {
-        initial: 0,
-        slides: {
-            perView: 4,
-            spacing: 10,
-        },
-    },
-    [ThumbnailPlugin(this.slider)]
-)
+
